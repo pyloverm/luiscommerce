e75921ce144dfa2aa563ac7b6700ce102f272216
pyloverm <ferreirabictor74200@gmail.com>
2022-12-15 14:15:51 +0000

    vercel mid update

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { Title } from '../src/data'
-const contacts = () => {
+import PocketBase from 'pocketbase';
+
+const contacts = ({data}) => {
     return(
-        <p>{Title()}</p>
+        <p>{data[0].nome}</p>
     )
 }
 
+
+// you can also fetch all records at once via getFullList
+
+
+export const getServerSideProps = async () => {
+    const pb = new PocketBase('https://poor-camera.pockethost.io');
+    const records = await pb.collection('products').getFullList(200, {sort: '-created',});
+    const data = JSON.parse(JSON.stringify(records))
+    return {
+      props: {data}
+    }
+  }
 export default contacts
