0348e215340d8ef6182789961047e0974e7db834
pyloverm <ferreirabictor74200@gmail.com>
2023-01-07 16:56:17 +0000

    buged contact page fixed

--- pages/contactos.js
+++ pages/contactos.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import { Title } from '../src/data'
 import PocketBase from 'pocketbase';
 
-const contacts = ({data}) => {
+const contactos = ({data}) => {
     return(
-        <p>{data[0].nome}</p>
+        <p>{data.items[0].nome}</p>
     )
 }
 
 
-// you can also fetch all records at once via getFullList
-
-
 export const getServerSideProps = async () => {
     const pb = new PocketBase('https://poor-camera.pockethost.io');
-    const records = await pb.collection('products').getFullList(200, {sort: '-created',});
+    const records = await pb.collection('products').getList(1,200, {sort: '-created',});
     const data = JSON.parse(JSON.stringify(records))
     return {
       props: {data}
     }
   }
-export default contacts
+export default contactos
