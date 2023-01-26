844ad787351031b0dd1938a39e341a43714c650d
pyloverm <ferreirabictor74200@gmail.com>
2023-01-26 18:14:47 +0000

    ds

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -12,15 +12,18 @@ export async function middleware(req){
     const urldash = req.nextUrl.clone()
     const urldemerde = req.nextUrl.clone()
     const urldemerde1 = req.nextUrl.clone()
+    const urldemerde2 = req.nextUrl.clone()
     urltrue.pathname = '/admin'
     urldash.pathname = '/dashboard/users'
     urldemerde.pathname = '/'+JSON.stringify(req.cookies.OursiteJWT)
+    
     urldemerde1.pathname = '/test'
+    urldemerde2.pathname = '/test2'
     if(url.includes("/admin")){
         console.log('in admin')
         if(jwt){
             try{
-                const user = await axios.post('/api/ver_jwt');
+                const user = await axios.get('/api/ver_jwt');
                 console.log('right jwt')
                 return NextResponse.redirect(urldash)
             }catch(e){
@@ -35,7 +38,7 @@ export async function middleware(req){
             return NextResponse.redirect(urldemerde)   
         }
         try{
-            const user = await axios.post('/api/ver_jwt');
+            const user = await axios.get('/api/ver_jwt');
             console.log('right jwt')
             return NextResponse.redirect(urldemerde1)   
             if(url.includes("/users")){
@@ -44,7 +47,7 @@ export async function middleware(req){
             return NextResponse.redirect(urldash)
         }catch(e){
             console.log('errror')
-            return NextResponse.redirect(urltrue)
+            return NextResponse.redirect(urldemerde2)
         }
     }
     return NextResponse.next()
